b62b5c2e074539bea6f8f5583912edb293457969
soonhyo <kshyo96@gmail.com>
2023-04-14 15:16:30 +0900

    Revert "update"
    This reverts commit 7c478ecd22c5dd44293ac05b854715755e38dd6b.

--- mycobot_280/scripts/calibration.py
+++ mycobot_280/scripts/calibration.py
@@ -1,168 +1,145 @@
+# mycobot_position_calibration.py
+#!/usr/bin/env python3
+import time, os, sys, signal, threading
+
+import numpy as np
+
 import rospy
 from sensor_msgs.msg import PointCloud2
-import open3d as o3d
-import numpy as np
-import ros_numpy
+from geometry_msgs.msg import Point
 import sensor_msgs.point_cloud2 as pc2
-import tf
-from pynput import keyboard
-import sys
 
-class PointCloudSubscriber:
+class MyCobotPositionCalibrationNode(object):
     def __init__(self):
-        self.num_points = 0
-        self.frame_id = ""
-        rospy.Subscriber("/hsi_color_filter/hsi_output", PointCloud2, self.callback)
-        self.listener = tf.TransformListener()
-        self.listener_cam = tf.TransformListener()
+        super(MyCobotPositionCalibrationNode, self).__init__()
+        rospy.init_node("mycobot_position_calibration_node")
+        rospy.loginfo("mycobot position calibration start")
 
-        self.camera_points = []
-        self.target_points = []
-        # Wait for the transformation between the robot's end effector and the base frame
-        self.listener.waitForTransform('/link1', '/link7', rospy.Time(), rospy.Duration(0.1))
-        self.listener_cam.waitForTransform('/camera_link', '/camera_color_optical_frame', rospy.Time(), rospy.Duration(0.1))
-        self.center = None
-        self.link_point = None
+    # Subscriber
+    def sub_pointcloud(self):
+        def callback(data):
+            self.data = data
+            rospy.loginfo("subscribed pointcloud")
+            xyz_generator = pc2.read_points(self.data, field_names = ("x", "y", "z"), skip_nans=True)
+            xyz_list = [xyz for xyz in xyz_generator if (abs(xyz[0]) < 0.8 and abs(xyz[1]) < 0.8 and abs(xyz[2]) < 0.8)]
+            xyz_array = np.array(xyz_list)
 
-        with keyboard.GlobalHotKeys({'q': self.my_function, 's': self.save_point}) as h:
-            h.join()
-    def dbscan(self, pcd):
-        with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
-            labels = np.array(pcd.cluster_dbscan(eps=0.2, min_points=10, print_progress=False))
-        max_label = labels.max()
-        # print(f"point cloud has {max_label + 1} clusters")
-        # Compute cluster centers
-        centers = []
-        for i in range(max(labels) + 1):
-            cluster_indices = np.where(labels == i)[0]
-            cluster_pcd = pcd.select_by_index(cluster_indices)
-            cluster_center = cluster_pcd.get_center()
-            centers.append(cluster_center)
-        # Assume camera position is at [0, 0, 0]
-        camera_position = np.array([0, 0, 0])
-        # Compute distance from camera to each cluster center
-        distances = [np.linalg.norm(camera_position - center) for center in centers]
+            if len(xyz_list) > 3:
+                marker_centroids = self.kmeans(3, xyz_array)
+                rospy.loginfo("\n marker positions\n{}".format(marker_centroids))
+                translation = self.cal_translation(marker_centroids)
+                rospy.loginfo("\n translation\n{}".format(translation))
 
-        # Find index of the closest cluster
-        closest_cluster_idx = np.argmin(distances)
+        sub = rospy.Subscriber("/hsi_color_filter/hsi_output", PointCloud2, callback = callback)
+        rospy.spin()
 
-        # Select the closest cluster
-        closest_cluster_indices = np.where(labels == closest_cluster_idx)[0]
-        closest_cluster_pcd = pcd.select_by_index(closest_cluster_indices)
-        return closest_cluster_pcd
+    # node start
+    def start_node(self):
+        sub_pointcloud = threading.Thread(target = self.sub_pointcloud)
+        sub_pointcloud.setDaemon(True)
+        sub_pointcloud.start()
+        sub_pointcloud.join()
 
-    def my_function(self):
-        print("Function executed!")
-        sys.exit()
+    # clustering method
+    def kmeans(self, k, X, max_iter=30): # k: cluster num, X: numpy array
+        data_size, n_features = X.shape
+        centroids = X[np.random.choice(data_size, k)]
+        new_centroids = np.zeros((k, n_features))
+        cluster = np.zeros(data_size)
+        for epoch in range(max_iter):
+            for i in range(data_size):
+                distances = np.sum((centroids - X[i]) ** 2, axis=1)
+                cluster[i] = np.argsort(distances)[0]
+            for j in range(k):
+                new_centroids[j] = X[cluster==j].mean(axis=0)
+            if np.sum(new_centroids == centroids) == k:
+                break
+            centroids = new_centroids
+        max_norm = 0
+        min_norm = 0
+        sorted_centroids = []
+        for centroid in centroids:
+            norm = centroid[2]
+            if norm > max_norm:
+                sorted_centroids.append(centroid)
+                max_norm = norm
+                if min_norm == 0:
+                    min_norm = sorted_centroids[0][2]
+            else:
+                if norm > min_norm and min_norm != 0:
+                    sorted_centroids.insert(1, centroid)
+                else:
+                    sorted_centroids.insert(0, centroid)
+                    min_norm = norm
+        sorted_centroids = np.array(sorted_centroids)
 
-    def save_point(self):
-        if self.center.any() and len(self.camera_points) < 4:
-            # print(self.center)
-            self.target_points.append(self.link_point)
-            self.camera_points.append(self.center)
-            print("point is saved")
+        return sorted_centroids
 
-            if len(self.camera_points) == 4:
-                print("4 points is saved, please continue to calculate the transform")
-                self.calculate_transform(self.camera_points, self.target_points)
-                self.camera_points = []
-                self.target_points = []
+    # translation angles calculation
+    ## calculation
+    def cal_translation(self, marker_points):
+        # マーカー1, 2, 3の位置ベクトル
+        a_1, a_2, a_3 = marker_points
+        # カメラからロボットへのベクトル
+        V_robot = self.cal_robot_position_vector(a_2, a_3)
+        # ロボットのXYZ単位ベクトル
+        V_X = (a_2 - V_robot) / (np.linalg.norm(a_2 - V_robot))
+        V_Y = (a_1 - V_robot) / (np.linalg.norm(a_1 - V_robot))
+        V_Z = self.cal_normal_vector(marker_points)
+        # カメラの水平面に対する仰角
+        theta_1 = - (np.pi/2 - self.cal_subtended_angle(-V_robot, V_Z))
+        # カメラの正面方向の回転角
+        V_Y_camera = np.array([0, 1, 0])
+        V_Y_camera_rotated = self.cal_rotate_vector_xaxis(V_Y_camera, -theta_1)
+        theta_2 = - self.cal_subtended_angle(V_Z, -V_Y_camera_rotated)
+        # カメラとロボットのそれぞれの正面方向とのなす角
+        _, V_robot_projected_to_plane = self.cal_vector_projection(V_robot, V_Z)
+        theta_3 = self.cal_subtended_angle(V_Y, V_robot_projected_to_plane)
+        # mycobotの位置を土台の高さ0.027 m, V_Z方向に平行移動
+        V_robot = V_robot + 0.027*V_Z
 
-    def rotation_matrix_to_rpy(self,R):
-        sy = np.sqrt(R[0,0]*R[0,0] + R[1,0]*R[1,0])
-        singular = sy < 1e-6
-        if not singular:
-            x = np.arctan2(R[2,1], R[2,2])
-            y = np.arctan2(-R[2,0], sy)
-            z = np.arctan2(R[1,0], R[0,0])
-        else:
-            x = np.arctan2(-R[1,2], R[1,1])
-            y = np.arctan2(-R[2,0], sy)
-            z = 0
+        return V_robot, theta_1, theta_2, theta_3
 
-        return np.array([x, y, z])
 
-    def calculate_transform(self, camera, link):
-        A = np.array(link).T
-        B = np.array(camera).T
-        A = np.vstack((A, np.ones((1,4))))
-        B = np.vstack((B, np.ones((1,4))))
+    ## vector and angle caluculation
+    def cal_robot_position_vector(self, a_2, a_3):
+        return (a_2 + a_3) / 2
 
-        # Calculate the transformation matrix using the method of least squares
-        # transform_matrix, residuals, rank, singular_values = np.linalg.lstsq(source_np_homogeneous, target_np_homogeneous, rcond=None
-        T = np.dot(A, np.linalg.inv(B))
+    def cal_normal_vector(self, marker_points):
+        a_1 = marker_points[0]
+        a_2 = marker_points[1]
+        a_3 = marker_points[2]
+        A_12 = a_2 - a_1
+        A_13 = a_3 - a_1
+        cross = np.cross(A_13, A_12)
+        return cross / np.linalg.norm(cross)
 
-        # Extract translation term
-        translation = T[:3, 3]
+    def cal_subtended_angle(self, vec_1, vec_2):
+        dot = np.dot(vec_1, vec_2)
+        norm_1 = np.linalg.norm(vec_1)
+        norm_2 = np.linalg.norm(vec_2)
+        return np.arccos( dot / (norm_1 * norm_2) )
 
-        # Extract rotation term
-        rotation = T[:3, :3]
-        rpy = self.rotation_matrix_to_rpy(rotation)
-        # Print the transformation matrix
-        print("rpy:\n",*rpy)
-        print("trans:\n",*translation)
-        # print(T)
-        return T
+    def cal_vector_projection(self, org_vec, normal_vec):
+        # org_vec: 射影したいベクトル
+        # normal_vec: 射影したい平面の法線ベクトル
+        projected_to_vertical = np.dot(org_vec, normal_vec) * normal_vec
+        projected_to_horizontal = org_vec + projected_to_vertical
+        return projected_to_vertical, projected_to_horizontal
 
-    def callback(self, msg):
-        # self.num_points = len(data.data) / data.point_step
-        self.frame_id = msg.header.frame_id
-        points = list(pc2.read_points(msg, skip_nans=True))
+    def cal_rotate_vector_xaxis(self, vec, angle):
+        rotate_mat = np.array([[1, 0, 0], [0, np.cos(angle), np.sin(angle)], [0, -np.sin(angle), np.cos(angle)]])
+        return vec.dot(rotate_mat)
 
-        try:
-            # Get the transformation from the end effector frame to the base frame
-            (trans, rot) = self.listener.lookupTransform('/link1', '/link7', rospy.Time(0))
-            (trans_cam, rot_cam) = self.listener_cam.lookupTransform('/camera_link', '/camera_color_optical_frame', rospy.Time(0))
+    def cal_rotate_vector_yaxis(self, vec, angle):
+        rotate_mat = np.array([[np.cos(angle), 0, -np.sin(angle)], [0, 1, 0], [np.sin(angle), 0, np.cos(angle)]])
+        return vec.dot(rotate_mat)
 
-            self.link_point = trans
-            # Print the translation vector (i.e. the coordinates of the end effector in the base frame)
-            # print(f"End effector coordinates: x = {trans[0]}, y = {trans[1]}, z = {trans[2]}")
+    def cal_rotate_vector_zaxis(self, vec, angle):
+        rotate_mat = np.array([[np.cos(angle), np.sin(angle), 0], [-np.sin(angle), np.cos(angle), 0], [0, 0, 1]])
+        return vec.dot(rotate_mat)
 
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            return
-
-        # Convert numpy array to Open3D point cloud
-
-        try:
-            pcd = o3d.geometry.PointCloud()
-            points = np.asarray(points, dtype=np.float32)[:,:3]
-            R =tf.transformations.quaternion_matrix(rot_cam)
-            TL =np.array(trans_cam).reshape(3,1)
-            TL = np.vstack((TL, np.ones((1,1))))
-            T_cam = np.hstack((R[:,:3], TL))
-
-            pcd.points = o3d.utility.Vector3dVector(points)
-            self.find_pointcloud_center(pcd, T_cam)
-            # print("Message frame ID: %s" % self.frame_id)
-
-        except Exception as e:
-            return
-
-    def find_pointcloud_center(self, pcd, T):
-        # Perform KNN clustering to find the center
-        # kdtree = o3d.geometry.KDTreeFlann(pcd)
-        # print(np.asarray(pcd.points))
-        # print(np.mean(pcd.points, axis=0))
-        # [k, idx, _] = kdtree.search_knn_vector_3d(np.mean(pcd.points, axis=0), 1)
-        # center = points[idx[0][0]]
-        # print(idx)
-        # center = np.asarray(pcd.points)[idx[0],:]
-        # center =np.mean(pcd.points, axis=0)
-        #DBSCAN for clustering
-        try:
-            pcd = self.dbscan(pcd)
-        except:
-            return
-
-        center = np.asarray(pcd.get_center())
-
-        # pcd_center = o3d.geometry.PointCloud()
-        # pcd_center.points = o3d.utility.Vector3dVector(np.array([center]))
-        # pcd_center.paint_uniform_color([1, 0, 0])
-
-        self.center = np.dot(T, np.append(center,1).T)[:3]
-
-if __name__=="__main__":
-    rospy.init_node('pointcloud_subscriber')
-    app = PointCloudSubscriber()
-    rospy.spin()
+if __name__ == "__main__":
+    mycobot_position_calibrator = MyCobotPositionCalibrationNode()
+    mycobot_position_calibrator.start_node()
+    pass
